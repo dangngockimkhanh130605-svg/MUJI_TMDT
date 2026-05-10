e8807a269aa7c3967f0726e0963c2bfe7b85b259
Đặng Ngọc Kim Khánh <dangngockimkhanh@kimkhanhne.local>
2026-05-11 00:23:29 +0700

    fix smtp

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,20 +1,31 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"MUJI Online Store" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    const info = await transporter.sendMail({
+      from: `"MUJI Online Store" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("MAIL SENT:", info.response);
+
+  } catch (err) {
+    console.log("MAIL ERROR:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
